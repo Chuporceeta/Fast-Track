d8131926722c625e16f2412c2ada4a47b5376a68
Chuporceeta <xabier.sienra@gmail.com>
2024-03-24 19:41:10 -0400

    Graph class and vertex visualizations with basic mouseover interactions.

--- graph.js
+++ graph.js
@@ -1,9 +1,47 @@
+const US_BOUNDS = {top:49.35,    // north lat
+                        left:-124.78, // west long
+                        right:-66.95, // east long
+                        bottom:24.74} // south lat
+
 class AdjList {
     constructor(data) {
-        this.cityData = data; // [{city, country, elevation, id, lat, lon, population, state, timezone}, ...]
+        this.cityData = new Map();
+        data.forEach((city) => {
+            this.cityData.set(city['id'], {lat:city['lat'], lon:city['lon'], name:city['city'], pop:city['population']});
+        });
+        console.log(this.cityData);
         this.graph = {}; // fromCityId : [toCityId, distance]
     }
 
-    
+    drawNodes(scale) {
+        let minPop = document.getElementById('pop-input').value;
+        let cities = Array.from(this.cityData).map(([id, data]) => (data));
+        cities = cities.filter((city) => {return city.pop > minPop;});
+        console.log(cities)
+        d3.select('#graph')
+            .selectAll('circle')
+            .data(cities)
+            .join('circle')
+            .on('mouseover', (e, d) => {
+                d3.select(e.target)
+                    .style('fill', 'red');
+            })
+            .on('mouseout', (e, d) => {
+                d3.select(e.target)
+                    .style('fill', '#dcdcdc');
+            })
+            .attr('cx', (city, i) => {
+                return 3236 * (city.lon + 124.78) / scale;
+            })
+            .attr('cy', (city, i) => {
+                return -4000 * (city.lat - 49.35) / scale;
+            })
+            .attr('r', (city, i) => {
+                return Math.log(city.pop-minPop)/3+3;
+            })
+            .style('fill', '#dcdcdc')
+            .style('stroke', '#000000')
+
+    }
 
 }
--- process-data.js
+++ process-data.js
@@ -1,7 +1,10 @@
+const svg = document.getElementById('graph-svg');
+let graph;
+
 init();
 
 async function getCityData() {
-    const response = await fetch("./us_cities.json");
+    const response = await fetch("us_cities.json");
     if (!response.ok)
         throw new Error(response.statusText);
     return response.json();
@@ -14,14 +17,29 @@ function fillCitiesList(data) {
         option.value = city['city'];
         cityList.append(option);
     });
+
+}
+
+function draw() {
+    graph.drawNodes(Math.max(190000/window.innerWidth, 100000/window.innerHeight));
+}
+
+function resizeSVG() {
+    svg.setAttribute('width', ''+window.innerWidth);
+    svg.setAttribute('height', ''+window.innerHeight);
+
+    draw();
 }
 
 function init() {
-    let graph;
+    window.addEventListener('resize', resizeSVG, false);
+
     const cityData = getCityData();
     cityData.then((data) => {
         console.log(data);
         fillCitiesList(data['data']);
         graph = new AdjList(data['data']);
+        resizeSVG();
+        //drawBorders(Math.max(190000/canvas.width, 100000/canvas.height));
     });
 }
