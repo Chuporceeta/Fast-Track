17434b34178d07243a2ba0cb95d20b5f5a852803
Chuporceeta <xabier.sienra@gmail.com>
2024-03-25 20:50:43 -0400

    Added US map, zooming, panning, animations on changing pop limit, and switched to using d3 Mercator projection

--- graph.js
+++ graph.js
@@ -1,47 +1,89 @@
-const US_BOUNDS = {top:49.35,    // north lat
-                        left:-124.78, // west long
-                        right:-66.95, // east long
-                        bottom:24.74} // south lat
-
 class AdjList {
     constructor(data) {
         this.cityData = new Map();
         data.forEach((city) => {
-            this.cityData.set(city['id'], {lat:city['lat'], lon:city['lon'], name:city['city'], pop:city['population']});
+            if (city['state'] !== 'AK' && city['state'] !== 'HI')
+                this.cityData.set(city['id'], {lat:city['lat'], lon:city['lon'], name:city['city'], pop:city['population']});
         });
-        console.log(this.cityData);
         this.graph = {}; // fromCityId : [toCityId, distance]
+
     }
 
-    drawNodes(scale) {
+    onResize() {
+        // Set up map projection
+        this.projection = d3.geoMercator()
+            //.rotate([0, 60, 0])
+            .fitExtent([[0,0], [window.innerWidth, window.innerHeight]], geojson);
+        this.geoGenerator = d3.geoPath()
+            .projection(this.projection);
+
+        // Draw Map
+        d3.select('#graph')
+            .selectAll('path')
+            .data(geojson['features'])
+            .join('path')
+            .attr('d', this.geoGenerator)
+            .style('fill', 'darkgray')
+            .style('stroke', 'black');
+
+    }
+
+    draw() {
+        let radius = (id) => Math.log(this.cityData.get(id).pop-minPop)/3+3;
         let minPop = document.getElementById('pop-input').value;
-        let cities = Array.from(this.cityData).map(([id, data]) => (data));
-        cities = cities.filter((city) => {return city.pop > minPop;});
-        console.log(cities)
+
+        // Get list of cities to display
+        let cityList = Array.from(this.cityData).map(([id, data]) => id);
+        cityList = cityList.filter(id => this.cityData.get(id).pop > minPop);
+
+        let transform = d3.zoomTransform(d3.select('#graph > path').node()) ?? d3.zoomIdentity;
+
+        // Draw Cities
         d3.select('#graph')
             .selectAll('circle')
-            .data(cities)
-            .join('circle')
-            .on('mouseover', (e, d) => {
-                d3.select(e.target)
-                    .style('fill', 'red');
-            })
-            .on('mouseout', (e, d) => {
-                d3.select(e.target)
-                    .style('fill', '#dcdcdc');
-            })
-            .attr('cx', (city, i) => {
-                return 3236 * (city.lon + 124.78) / scale;
-            })
-            .attr('cy', (city, i) => {
-                return -4000 * (city.lat - 49.35) / scale;
-            })
-            .attr('r', (city, i) => {
-                return Math.log(city.pop-minPop)/3+3;
-            })
-            .style('fill', '#dcdcdc')
-            .style('stroke', '#000000')
+            .data(cityList, d => d)
+            .join(
+                (enter) => {
+                    return enter
+                        .append('circle')
+                        .on('mouseover', (e, d) => {
+                            d3.select(e.target)
+                                .style('fill', 'red');
+                        })
+                        .on('mouseout', (e, d) => {
+                            d3.select(e.target)
+                                .style('fill', '#dcdcdc');
+                        })
+                        .attr('r', 0)
+                        .style('fill', '#dcdcdc')
+                        .style('stroke', '#000000')
+                      ;
 
+                },
+                (update) => {
+                    return update;
+                },
+                (exit) => {
+                    return exit
+                        .transition()
+                        .ease(d3.easeBackIn)
+                        .attr('r', 0)
+                        .remove();
+                }
+            )
+            .attr('cx', (id, i) => {
+                let city = this.cityData.get(id);
+                return this.projection([city.lon, city.lat])[0];
+            })
+            .attr('cy', (id, i) => {
+                let city = this.cityData.get(id);
+                return this.projection([city.lon, city.lat])[1];
+            })
+            .attr('transform', function(id, i) {
+                return translateOnly(d3.select(this), transform);
+            })
+            .transition()
+            .attr('r', id => radius(id));
     }
 
 }
--- process-data.js
+++ process-data.js
@@ -1,10 +1,18 @@
 const svg = document.getElementById('graph-svg');
-let graph;
+let graph, geojson;
 
 init();
 
 async function getCityData() {
     const response = await fetch("us_cities.json");
+    if (!response.ok)
+        throw new Error(response.statusText);
+    return response.json();
+}
+
+async function getUSBorders() {
+    // Data from https://github.com/ResidentMario/geoplot-data/blob/master/contiguous-usa.geojson
+    const response = await fetch('contiguous-usa.geojson');
     if (!response.ok)
         throw new Error(response.statusText);
     return response.json();
@@ -20,26 +28,46 @@ function fillCitiesList(data) {
 
 }
 
-function draw() {
-    graph.drawNodes(Math.max(190000/window.innerWidth, 100000/window.innerHeight));
-}
-
 function resizeSVG() {
     svg.setAttribute('width', ''+window.innerWidth);
     svg.setAttribute('height', ''+window.innerHeight);
 
-    draw();
+    graph.onResize();
+    graph.draw();
+}
+
+function translateOnly(circle, transform) {
+    return "translate(" + (circle.attr('cx')*(transform.k-1)+transform.x) + ", " + (circle.attr('cy')*(transform.k-1)+transform.y) +")";
+}
+
+function setUpZoom() {
+    // Set up zoom and pan
+    let zoom = d3.zoom()
+        .on('zoom', e => {
+            d3.select('#graph')
+                .selectAll('path')
+                .attr('transform', e.transform);
+            d3.select('#graph')
+                .selectAll('circle')
+                .attr('transform', function(id, i) {
+                    return translateOnly(d3.select(this), e.transform);
+                });
+        });
+    d3.select('#graph-svg').call(zoom);
 }
 
 function init() {
     window.addEventListener('resize', resizeSVG, false);
 
-    const cityData = getCityData();
-    cityData.then((data) => {
-        console.log(data);
-        fillCitiesList(data['data']);
-        graph = new AdjList(data['data']);
+    const cityDataP = getCityData();
+    const borderDataP = getUSBorders();
+
+    Promise.all([cityDataP, borderDataP])
+    .then(([cityData, borderData]) => {
+        fillCitiesList(cityData['data']);
+        graph = new AdjList(cityData['data']);
+        geojson = borderData;
+        setUpZoom();
         resizeSVG();
-        //drawBorders(Math.max(190000/canvas.width, 100000/canvas.height));
     });
 }
