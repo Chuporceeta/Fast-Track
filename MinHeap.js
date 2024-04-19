78fffb7240003b4105865129bdd6df1220d08843
Chuporceeta <xabier.sienra@gmail.com>
2024-04-19 15:15:15 -0400

    Frame for MinHeap class

---
+++ MinHeap.js
@@ -0,0 +1,18 @@
+class MinHeap {
+    constructor(valueFunction) {
+        this.content = [];
+        this.value = valueFunction; // Compare using this.value(element) not just element
+    }
+
+    size() {
+        return this.content.length;
+    }
+
+    push() {
+
+    }
+
+    pop() {
+
+    }
+}
