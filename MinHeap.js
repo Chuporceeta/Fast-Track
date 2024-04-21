1348c16b45139d6d3c4261bc308e3e4a5b5c2c9a
carlosf02 <92893194+carlosf02@users.noreply.github.com>
2024-04-20 20:35:14 -0400

    Update MinHeap.js

--- MinHeap.js
+++ MinHeap.js
@@ -9,10 +9,67 @@ class MinHeap {
     }
 
     push() {
-
+        this.content.push(element);
+        this.bubbleUp(this.content.length - 1);
     }
 
     pop() {
+        const result = this.content[0];
+        const end = this.content.pop();
+        if (this.content.length > 0) {
+            this.content[0] = end;
+            this.sinkDown(0);
+        }
+        return result;
+    }
 
+    bubbleUp(n) {
+        const element = this.content[n];
+        const score = this.value(element);
+        while (n > 0) {
+            const parentN = Math.floor((n + 1) / 2) - 1;
+            const parent = this.content[parentN];
+            if (score >= this.value(parent)) {
+                break;
+            }
+            this.content[parentN] = element;
+            this.content[n] = parent;
+            n = parentN;
+        }
+    }
+
+    sinkDown(n) {
+        const length = this.content.length;
+        const element = this.content[n];
+        const elemScore = this.value(element);
+
+        while (true) {
+            const child2N = (n + 1) * 2;
+            const child1N = child2N - 1;
+
+            let swap = null;
+            let child1Score;
+            // If the first child exists (is inside the array)...
+            if (child1N < length) {
+                const child1 = this.content[child1N];
+                child1Score = this.value(child1);
+                if (child1Score < elemScore) {
+                    swap = child1N;
+                }
+            }
+            // Do the same checks for the other child.
+            if (child2N < length) {
+                const child2 = this.content[child2N];
+                const child2Score = this.value(child2);
+                if (child2Score < (swap === null ? elemScore : child1Score)) {
+                    swap = child2N;
+                }
+            }
+
+            if (swap === null) break;
+            this.content[n] = this.content[swap];
+            this.content[swap] = element;
+            n = swap;
+        }
     }
 }
